ca74cf3ae5460cf3b08e4d78a5227fbf53f368ae
Lucas <lucas.senoville@outlook.fr>
2024-10-07 14:38:06 +0200

    first commit

---
+++ src/main/java/com/renfo_backend/Renfo_backend/RenfoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.renfo_backend.Renfo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenfoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenfoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renfo_backend/Renfo_backend/RenfoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renfo_backend.Renfo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenfoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
